f50a419387000c0e6cfb93dfe7d1be0df2da2b3c
cademerritt <39072260+cademerritt@users.noreply.github.com>
2021-10-24 18:56:55 -0400

    Add files via upload

---
+++ slots/index.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Slot Machine</h1>
+        <Machine s1="🍊" s2="🍊" s3="🍊" />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
